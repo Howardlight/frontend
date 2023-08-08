d1f35eddd7fe689add14e0e115656055b83afaa0
Mhammad Nazir <nazirchase@gmail.com>
2023-08-09 00:35:48 +0300

    Turned off refetchOnFocus + Replaced hardcoded url with env

--- app/redux/services/taskApi.ts
+++ app/redux/services/taskApi.ts
@@ -3,9 +3,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const TaskAPI = createApi({
     reducerPath: 'taskApi',
-    refetchOnFocus: true,
+    // refetchOnFocus: true,
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:5000/api/"
+        baseUrl: process.env.NEXT_PUBLIC_BACKEND_URL + "/api/"
     }),
     endpoints: (builder) => ({
         getTasks: builder.query<Task[], null>({
