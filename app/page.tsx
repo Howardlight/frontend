40110367ea6f72f82aeecd41ce9908613f11d076
Mhammad Nazir <nazirchase@gmail.com>
2023-08-08 23:35:17 +0300

    Added no-cache for fetching tasks

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 async function getTasks() {
   //TODO: Replace this with local.env
-  const res = await fetch(`${process.env.BACKEND_URL}/api/task`, { method: "GET" });
+  const res = await fetch(`${process.env.BACKEND_URL}/api/task`, { method: "GET", cache: "no-cache" });
 
   //Triggers closest error.js 
   //TODO: Create an Error.js for when Tasks could not be loaded
