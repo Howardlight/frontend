f6c9f893bf92961eaa399984f7336cb46677f17f
Mhammad Nazir <nazirchase@gmail.com>
2023-08-09 00:25:40 +0300

    Integrated TaskAPI to Store

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,15 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
 
 import taskReducer from "./TasksSlice";
+import { taskApi } from "./services/taskApi";
 
 const reducers = combineReducers({
-    taskReducer
+    taskReducer,
+    [taskApi.reducerPath]: taskApi.reducer
 })
 
 export const store = configureStore({
-    reducer: reducers
-})
+    reducer: reducers,
+    devTools: process.env.NODE_ENV !== `production`,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({}).concat([taskApi.middleware]),
+});
+
+setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
