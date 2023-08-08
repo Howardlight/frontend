6c436b1a34563150da3d77c101a876b6dcfc6699
Mhammad Nazir <nazirchase@gmail.com>
2023-08-08 23:35:39 +0300

    Mapped over all tasks in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+import TaskCard from "@/components/Task";
+import { Task as TaskType } from "@/types/Task";
+
 async function getTasks() {
   //TODO: Replace this with local.env
   const res = await fetch(`${process.env.BACKEND_URL}/api/task`, { method: "GET", cache: "no-cache" });
@@ -15,10 +18,18 @@ async function getTasks() {
 export default async function Home() {
   const data = await getTasks();
 
+  //TODO: Convert this to a grid
+
   console.log(data);
   return (
     <main>
-
+      <div className="flex flex-row gap-2 justify-center flex-wrap">
+        <Tasks tasksData={data} />
+      </div>
     </main>
   )
+}
+
+function Tasks({ tasksData }: { tasksData: TaskType[] }) {
+  return tasksData.map(task => <TaskCard key={task._id} task={task} />)
 }
