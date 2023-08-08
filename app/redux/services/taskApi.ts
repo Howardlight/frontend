6a5020a55d13f9aa2864bf55f136cdb9655ab94e
Mhammad Nazir <nazirchase@gmail.com>
2023-08-09 00:25:33 +0300

    Created TaskAPI

---
+++ app/redux/services/taskApi.ts
@@ -0,0 +1,17 @@
+import { Task } from "@/types/Task";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const taskApi = createApi({
+    reducerPath: 'taskApi',
+    refetchOnFocus: true,
+    baseQuery: fetchBaseQuery({
+        baseUrl: "http://localhost:5000/api/"
+    }),
+    endpoints: (builder) => ({
+        getTasks: builder.query<Task[], null>({
+            query: () => `task`
+        }),
+    }),
+})
+
+export const { useGetTasksQuery } = taskApi;
