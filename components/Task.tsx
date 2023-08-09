c0a83407e39f1f993c3d29b3f19e11a00ba95cee
Mhammad Nazir <nazirchase@gmail.com>
2023-08-09 06:20:33 +0300

    Added Delete Modal to Task

--- components/Task.tsx
+++ components/Task.tsx
@@ -2,9 +2,12 @@
 import { Task } from "@/types/Task";
 import { Fragment, useState } from "react";
 import { EditModal } from "./modals/EditModal";
+import DeleteModal from "./modals/DeleteModal";
+import { IconEdit, IconTrash } from "@tabler/icons-react";
 
 export default function TaskCard({ task }: { task: Task }) {
     let [isEditModalOpen, setIsEditModalOpen] = useState(false);
+    let [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
     function openModal() {
         setIsEditModalOpen(true);
@@ -16,14 +19,24 @@ export default function TaskCard({ task }: { task: Task }) {
 
     return (
         <Fragment>
-            <div className="w-80 bg-gray-100 shadow-sm p-3 rounded-md">
-                <div className="flex flex-row justify-between mb-5">
-                    <p className="font-semibold">{task.title}</p>
-                    <button onClick={openModal}>Edit</button>
+            <div className="flex flex-col justify-between w-80 bg-gray-100 shadow-sm p-3 rounded-md">
+                <div>
+
+                    <div className="flex flex-row justify-between mb-5">
+                        <p className="font-semibold">{task.title}</p>
+
+                        <button onClick={openModal}><IconEdit /></button>
+                    </div>
+                    <p>{task.description}</p>
+
                 </div>
-                <p>{task.description}</p>
+
+                <div className="flex flex-row justify-end pt-4">
+                    <button onClick={() => setIsDeleteModalOpen(true)}><IconTrash /></button>
+                </div>
             </div>
             <EditModal isOpen={isEditModalOpen} closeModal={closeModal} task={task} />
+            <DeleteModal isOpen={isDeleteModalOpen} setIsOpen={setIsDeleteModalOpen} task={task} />
         </Fragment>
     )
 }
