da49b5ff8b475b5de15506255e349bb72ce43104
Mhammad Nazir <nazirchase@gmail.com>
2023-08-09 05:07:19 +0300

    Added small navbar

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,13 @@ export default function Home() {
   if (isLoading || isFetching) return <p>Loading...</p>;
   if (error) return <p>An error occured</p>;
   return (
-    <main>
-      <div className="flex flex-row gap-2 justify-center flex-wrap">
+    <main className="m-5 h-full">
+      <div className="flex flex-row justify-between p-2 mb-5 bg-gray-100 rounded-sm">
+        <p className="text-lg font-bold">Task Manager</p>
+        <button>Create</button>
+      </div>
+      {/* <div className="flex flex-row gap-2 justify-center flex-wrap"> */}
+      <div className="grid grid-flow-row grid-cols-3 gap-4">
         <Tasks tasksData={data!} />
       </div>
     </main>
